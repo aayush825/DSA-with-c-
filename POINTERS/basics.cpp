4649de6046d94558bca9f0de0847c59761ff2e30
Ayush Kumar <ayushkumar250801@gmail.com>
2022-10-30 12:35:57 +0530

    ..

---
+++ POINTERS/basics.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ int num=5;
+ 
+ cout<<"address of num"<<&num<<endl;
+
+ int *ptr = &num; 
+
+ cout<<"address is"<<ptr<<endl;
+ cout<<"value is"<<*ptr<<endl;
+
+ double d=4.3;
+ double *p2 = &d;
+
+ cout<<"address is"<<p2<<endl;
+ cout<<"value is"<<*p2<<endl;
+
+}
