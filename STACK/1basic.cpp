3fd01f37244b09b8ce41e2a21c37373ad6eeeb71
Ayush Kumar <ayushkumar250801@gmail.com>
2022-11-22 18:26:04 +0530

    Create 1basic.cpp

---
+++ STACK/1basic.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+    //using stl
+    //creation of stack with name s
+    stack<int> s;
+
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.pop();
+
+    cout<<"printing top element  "<<s.top()<<endl;
+    if(s.empty()){
+        cout<<"stack is empty"<<endl;
+    }
+    else{
+        cout<<"stack is not empty"<<endl;
+    }
+    cout<<"size of stack is "<<s.size()<<endl;
+
+
+    return 0;
+
+}
