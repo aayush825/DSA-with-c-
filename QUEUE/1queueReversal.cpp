4d367c8d58db287d51d107336bcd07fb88181806
Ayush Kumar <ayushkumar250801@gmail.com>
2022-11-29 00:11:49 +0530

    Create 1queueReversal.cpp

---
+++ QUEUE/1queueReversal.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<queue>
+#include<stack>
+using namespace std;
+
+queue<int> rev(queue<int> q){
+    stack<int> s;
+
+    while(!q.empty()){
+        int element = q.front();
+        q.pop();
+        s.push(element);
+    }
+    while(!s.empty()){
+        int element = s.top();
+        s.pop();
+        q.push(element);
+    }
+    return q;
+}
