48da8f9f481856053cdc9a1f7202a84f65d5ee4a
Ayush Kumar <ayushkumar250801@gmail.com>
2022-12-13 09:48:36 +0530

    Create MaxFrequencyNumber.cpp

---
+++ MAP/MaxFrequencyNumber.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maximumFrequency(vector<int> &arr,int n){
+
+    unordered_map<int,int> count;
+
+    int maxFreq = 0;
+    int maxAns = 0;
+
+    for(int i =0; i<arr.size();i++){
+        count[arr[i]]++; //jo bhi arr m pada h usko increment krdo
+        maxFreq = max(maxFreq,count[arr[i]]);
+    }
+  
+    for(int i =0;i<arr.size();i++){
+        if(maxFreq == count[arr[i]]){
+            maxAns = arr[i];
+            break;
+        }
+    }
+    return maxAns;
+} 
