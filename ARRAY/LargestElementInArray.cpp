0cb371f0b00ff5af3af86720f8018771c8a24d42
Ayush Kumar <ayushkumar250801@gmail.com>
2022-12-28 14:49:58 +0530

    Create LargestElementInArray.cpp

---
+++ ARRAY/LargestElementInArray.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int largest(vector<int> &vect){
+int max = vect[0];
+for(int i=0;i<vect.size();i++){
+    if(max < vect[i]){
+        max = vect[i];
+    }
+}
+
+cout<<"vale of temp"<<max;
+}
+
+int main(){
+vector<int> vect{2,6,6,9,54,10,65,101,201};
+
+largest(vect);
+
+
+}
