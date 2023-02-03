b55c0da00881409ed18295c38d41a4646fbe7950
Ayush Kumar <67095509+aayush825@users.noreply.github.com>
2023-02-03 09:55:52 +0530

    Update 1.FACTORIAL USING  RECURSION.cpp

--- RECURSION/1.FACTORIAL USING  RECURSION.cpp
+++ RECURSION/1.FACTORIAL USING  RECURSION.cpp
@@ -4,14 +4,12 @@ using namespace std;
 int fact(int n){
     if(n == 0)
       return 1;
-      
-    
-    return n * fact(n-1);
-    
-    
+        
+    return n * fact(n-1);   
 }
+
 int main() {
-    // Write C++ code here
+    
     int n;
     cout<<"enter no";
     cin>> n ;
