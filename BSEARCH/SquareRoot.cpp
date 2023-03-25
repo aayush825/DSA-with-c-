0479860a1bfbc26e3d688d6d10b0935cf8cf3b14
Ayush Kumar <ayushkumar250801@gmail.com>
2023-03-25 07:49:15 +0530

    Create SquareRoot.cpp

---
+++ BSEARCH/SquareRoot.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long int sqrtInteger(int n){
+        int s =0;
+        int e = n;
+        long long int mid = s+(e-s)/2;
+        long long int ans  =-1;
+        while(s <= e){
+            long long int square = mid*mid;
+
+            if(square == n)
+                return mid;
+            if(square < n){
+                ans = mid;
+                s = mid+1;
+            }    
+            else{
+                e = mid -1;
+            }
+            mid = s+(e-s)/2;
+        }
+
+        return ans;
+    }
+
+double morePrecision(int n,int precision,int tempsol){
+    double factor = 1;
+    double ans = tempsol;
+
+    for(int i=0;i<precision;i++){
+        factor = factor/10;
+
+        for(double j=ans; j*j<n; j = j+factor){
+            ans = j;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    int n;
+    cout<<"enter the number"<<endl;
+    cin>>n;
+
+    int tempSol = sqrtInteger(n);
+
+    cout<<"Answer is "<<morePrecision(n,3,tempSol);  
+
+    return 0;  
+
+}
+    
