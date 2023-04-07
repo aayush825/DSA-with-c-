b7b21ac3370ed3b76218b4241db380506051b20b
Ayush Kumar <ayushkumar250801@gmail.com>
2023-04-07 14:45:59 +0530

    Create ValidAnaggram.cpp

---
+++ STRING/ValidAnaggram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n1 = s.length();
+        int n2 = t.length();
+
+        if(n1 != n2)
+            return false;
+
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+
+        for(int i =0;i<t.length();i++){
+            if(s[i] != t[i])
+                return false;  
+        }
+        return true; 
+    }
+};
