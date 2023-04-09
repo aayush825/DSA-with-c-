5e1cc1a914744205b010ab22a9e6811311e5ac10
Ayush Kumar <ayushkumar250801@gmail.com>
2023-04-09 16:58:10 +0530

    Create optimisedPower.cpp

---
+++ RECURSION/optimisedPower.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int power(int a,int b){
+    if(b == 0)
+        return 1;
+    if(b == 1)
+        return a;
+
+    int ans = power(a,b/2);
+
+    //if b even
+    if( b%2 == 0){
+        return ans*ans;
+    }   
+    else{
+        return a * ans*ans;
+    }     
+}
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<endl;
+    cout<< power(a,b);
+}
